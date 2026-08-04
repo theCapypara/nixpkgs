bad8dd4f819f40652cdcea5276364911ce628f15
Marco "Capypara" Köpcke <hello@capypara.de>
2026-08-04 18:29:09 +0200

    gnome-photos: drop
    Package is part of the GNOME archive and no longer maintained.

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -1021,6 +1021,7 @@ mapAliases {
   gnome-icon-theme = throw "'gnome-icon-theme' has been removed because it is unmaintained upstream and depends on GTK 2."; # Added 2026-07-22
   gnome-nettool = throw "'gnome-nettool' has been removed as its part of the GNOME archive and no longer maintained."; # Added 2026-08-04
   gnome-passwordsafe = throw "'gnome-passwordsafe' has been renamed to/replaced by 'gnome-secrets'"; # Converted to throw 2025-10-27
+  gnome-photos = throw "'gnome-photos' has been removed as its part of the GNOME archive and no longer maintained."; # Added 2026-08-04
   gnome-recipes = throw "'gnome-recipes' has been removed due to lack of upstream maintenance and dependency on insecure libraries"; # Added 2025-09-06
   gnome-resources = throw "'gnome-resources' has been renamed to/replaced by 'resources'"; # Converted to throw 2025-10-27
   gnome-settings-daemon46 = throw "'gnome-settings-daemon46' has been removed, no longer used by Pantheon"; # Added 2026-01-24
